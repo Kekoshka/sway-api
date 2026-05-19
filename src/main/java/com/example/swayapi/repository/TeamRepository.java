49feff6d3b628a6e3f55fd343cea8267b9bd11cd
masshuka <msipicyna96@gmail.com>
2026-05-19 23:06:04 +0500

    Update TeamRepository.java

--- src/main/java/com/example/swayapi/repository/TeamRepository.java
+++ src/main/java/com/example/swayapi/repository/TeamRepository.java
@@ -2,8 +2,10 @@ package com.example.swayapi.repository;
 
 import com.example.swayapi.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.Optional;
 public interface TeamRepository extends JpaRepository<Team, Long> {
     Optional<Team> findByCode(String code);
     Optional<Team> findByOwnerId(String ownerId);
+    List<Team> findAllByOwnerId(String ownerId);
 }
