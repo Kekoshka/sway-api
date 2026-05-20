6228372d14da2299f7783092efc5af0f9f10d8f0
masshuka <msipicyna96@gmail.com>
2026-05-20 16:05:11 +0500

    Update TeamMemberRepository.java

--- src/main/java/com/example/swayapi/repository/TeamMemberRepository.java
+++ src/main/java/com/example/swayapi/repository/TeamMemberRepository.java
@@ -2,11 +2,19 @@ package com.example.swayapi.repository;
 
 import com.example.swayapi.model.TeamMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
+
 public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
-    void deleteByTeamId(Long teamId);
     List<TeamMember> findByTeamId(Long teamId);
     Optional<TeamMember> findByUserId(String userId);
     Optional<TeamMember> findByTeamIdAndUserId(Long teamId, String userId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM TeamMember m WHERE m.teamId = :teamId")
+    void deleteByTeamId(Long teamId);
 }
